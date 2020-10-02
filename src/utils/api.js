bdab17a76d4a13c2fbe5ba3c38c60e34cfbeae91
Ana Lucia Chin <ananime@gmail.com>
2020-10-02 10:41:39 -0400

    created environment variables and added the .env file to gitignore

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,8 @@ export function fetchMovieReview (start_date, end_date) {
 
 	//console.log(start_date)
 	//console.log(end_date)
-	const endpoint = window.encodeURI(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?opening-date=${start_date};${end_date}&api-key=${apiConfig.movieReviewKey}`)
+
+	const endpoint = window.encodeURI(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?opening-date=${start_date};${end_date}&api-key=${process.env.REACT_APP_MOVIE_REVIEW_KEY}`)
 
 	return fetch(endpoint)
 		.then(response => {
