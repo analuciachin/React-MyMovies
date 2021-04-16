22287cdd567b74c60eb46c1c75d4ac628df5a56a
Ana Lucia Chin <ananime@gmail.com>
2021-04-16 10:20:32 -0400

    change ; for : in endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,29 +1,27 @@
+export function fetchMovieReview(start_date, end_date) {
+  //console.log(start_date)
+  //console.log(end_date)
 
-export function fetchMovieReview (start_date, end_date) {
+  const endpoint = window.encodeURI(
+    `https://api.nytimes.com/svc/movies/v2/reviews/picks.json?opening-date=${start_date}:${end_date}&api-key=${process.env.REACT_APP_MOVIE_REVIEW_KEY}`
+  );
 
-	//console.log(start_date)
-	//console.log(end_date)
-
-	const endpoint = window.encodeURI(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?opening-date=${start_date};${end_date}&api-key=${process.env.REACT_APP_MOVIE_REVIEW_KEY}`)
-
-	return fetch(endpoint)
-		.then(response => {
-			if(!response.ok) {
-				throw new Error();
-			}
-			return response.json();
-		})
+  return fetch(endpoint).then((response) => {
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  });
 }
 
-
-export function simulateLogin (username, password) {
-	return new Promise((resolve, reject) => {
-	  setTimeout(() => {
-		if(username === 'abc' && password === '123') {
-		  resolve();
-		} else {
-		  reject();
-		}
-	  }, 3000);
-	});
-  }
+export function simulateLogin(username, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (username === "abc" && password === "123") {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 3000);
+  });
+}
